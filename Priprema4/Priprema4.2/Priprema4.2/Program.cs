9397aec19e7fa7f6d384adf3e66285838925cf8d
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-24 19:03:11 +0100

    Add files via upload

---
+++ Priprema4/Priprema4.2/Priprema4.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = { 1, 2, 3, 4, 5, 6};
+            int[] niz2 = { 1, 8, 9, 4, 5, 9};
+            int x42, count = 0;
+            if (niz1.Length < niz2.Length)
+            {
+                x42 = niz1.Length;
+            }
+            else
+            {
+                x42 = niz2.Length;
+            }
+            for (int i = 0; i < x42; i++)
+            {
+                if (niz1[i] == niz2[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Broj zajedničkih članova je: "+ count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema4/Priprema4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = new int[10];
+            int[] niz2 = new int[10];
+            int br, n1 = 0, n2 = 0;
+            do
+            {
+                Console.WriteLine("Upišite broj:");
+                br = Convert.ToInt32(Console.ReadLine());
+                if (br % 2 != 0)
+                    niz1[n1++] = br;
+                else
+                    niz2[n2++] = br;
+            } while (br != 0 && n1 < 10 && n2 < 10);
+            Console.WriteLine();
+            for (int i = 0; i < n1; i++)
+            {
+                Console.WriteLine(niz1[i]);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < n2; i++)
+            {
+                Console.WriteLine(niz2[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
